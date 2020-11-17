fb8a830d1c00c56396a3fb68e2d25761eb007534
Zhafran Zhabyansyah <zhafranku@gmail.com>
2020-11-17 11:30:27 +0700

    add redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import Counter from './Counter';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Counter />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
